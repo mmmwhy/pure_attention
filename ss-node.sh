45c7534eadc7a48f19f963fdb29ba9a4f9feeb1d
Wing lee <mmmwhy@hust.edu.cn>
2017-05-09 13:00:54 +0800

    no message

---
+++ ss-node.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+clear
+echo
+echo "#############################################################"
+echo "# One click Install Shadowsocks-Python-Manyuser             #"
+echo "# Github: https://github.com/mmmwhy/ss-panel-ss-py-mu       #"
+echo "# Author: Feiyang.li                                        #"
+echo "# Blog: https://feiyang.li/                                 #"
+echo "#############################################################"
+echo
+read -p "Please input your domain(like:https://ss.feiyang.li or http://114.114.114.114): " Userdomain
+read -p "Please input your mukey(like:mupass): " Usermukey
+read -p "Please input your Node_ID(like:3): " UserNODE_ID
+#检查系统
+#check OS version
+check_sys(){
+	if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+	bit=`uname -m`
+}
+install_soft_for_each(){
+	check_sys
+	if [[ ${release} = "centos" ]]; then
+		echo "Will install below software on your centos system:"
+		yum install git -y
+		yum install python-setuptools -y 
+		yum -y groupinstall "Development Tools" -y
+		wget https://raw.githubusercontent.com/mmmwhy/ss-panel-ss-py-mu/master/libsodium-1.0.11.tar.gz #origin trace was baned in china
+		tar xf libsodium-1.0.11.tar.gz && cd libsodium-1.0.11
+		./configure && make -j2 && make install
+		echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+		ldconfig
+		yum install python-setuptools
+		easy_install supervisor
+	else
+	apt-get update
+	apt-get install supervisor
+	apt-get install git -y
+	apt-get install build-essential
+	wget https://raw.githubusercontent.com/mmmwhy/ss-panel-ss-py-mu/master/libsodium-1.0.11.tar.gz #origin trace was baned in china
+	tar xf libsodium-1.0.11.tar.gz && cd libsodium-1.0.11
+	./configure && make -j2 && make install
+	ldconfig
+	fi
+	echo -e "依赖安装完成！"
+}
+install_soft_for_each
+echo "Let's setup your ssnode/root"
+git clone https://github.com/mmmwhy/shadowsocks-py-mu.git "/root/shadowsocks-py-mu"
+cp /root/shadowsocks-py-mu/shadowsocks/config_example.py /root/shadowsocks-py-mu/shadowsocks/config.py
+#modify Config.py
+echo -e "modify Config.py...\n"
+Userdomain=${Userdomain:-"https://ss.feiyang.li"}
+sed -i "s#http://domain#${Userdomain}#" /root/shadowsocks-py-mu/shadowsocks/config.py
+Usermukey=${Usermukey:-"mupass"}
+sed -i "s#mupass#${Usermukey}#" /root/shadowsocks-py-mu/shadowsocks/config.py
+UserNODE_ID=${UserNODE_ID:-"3"}
+sed -i "s#'1'#'${UserNODE_ID}'#" /root/shadowsocks-py-mu/shadowsocks/config.py
+echo_supervisord_conf > /etc/supervisord.conf
+sed -i '$a [program:ss-manyuser]\ncommand = python /root/shadowsocks-py-mu/shadowsocks/servers.py\nuser = root\nautostart = true\nautorestart = true' /etc/supervisord.conf
+supervisord
+iptables -I INPUT -p tcp -m tcp --dport 104 -j ACCEPT
+iptables -I INPUT -p tcp -m tcp --dport 1024: -j ACCEPT
+iptables-save
+sleep 4
+cat shadowsocks.log
