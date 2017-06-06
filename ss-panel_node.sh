cee6fbbf2b4674e23b38de34a7118447d0bb6257
Wing lee <mmmwhy@qq.com>
2017-06-06 20:26:01 +0800

    Update ss-panel_node.sh

--- ss-panel_node.sh
+++ ss-panel_node.sh
@@ -126,7 +126,7 @@ install_ss_py_mu(){
 	iptables -I INPUT -p tcp -m tcp --dport 104 -j ACCEPT
 	iptables -I INPUT -p tcp -m tcp --dport 1024: -j ACCEPT
 	service iptables save
-	echo "/usr/bin/supervisorctl start ss-manyuser" >> /etc/rc.d/rc.local
+	echo "supervisord" >> /etc/rc.d/rc.local
 	sleep 4
 	cat shadowsocks.log
 }
@@ -192,7 +192,7 @@ one_click_all(){
 	iptables -I INPUT -p tcp -m tcp --dport 104 -j ACCEPT
 	iptables -I INPUT -p tcp -m tcp --dport 1024: -j ACCEPT
 	iptables-save
-	echo "/usr/bin/supervisorctl start ss-manyuser" >> /etc/rc.d/rc.local
+	echo "supervisord" >> /etc/rc.d/rc.local
 	sleep 4
 	cat shadowsocks.log
 	echo ""
