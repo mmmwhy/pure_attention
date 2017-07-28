141f11b44abdb50a98710c577e5905f4ee1dcc99
Wing lee <mmmwhy@qq.com>
2017-07-28 18:57:45 +0800

    Update ss-panel-v3-mod.sh

--- ss-panel-v3-mod.sh
+++ ss-panel-v3-mod.sh
@@ -75,7 +75,7 @@ install_centos_ssr(){
 	ldconfig
 	#clone shadowsocks
 	cd /root
-	git clone -b manyuser https://github.com/glzjin/shadowsocks.git "/root/shadowsocks"
+	git clone -b aead https://github.com/glzjin/shadowsocks.git "/root/shadowsocks"
 	#install devel
 	cd /root/shadowsocks
 	yum -y install python-devel
@@ -96,7 +96,7 @@ install_centos_ssr(){
 }
 install_ubuntu_ssr(){
 	apt-get install build-essential wget -y
-	apt-get install iptables -y
+	apt-get install iptables git -y
 	wget https://github.com/jedisct1/libsodium/releases/download/1.0.13/libsodium-1.0.13.tar.gz
 	tar xf libsodium-1.0.13.tar.gz && cd libsodium-1.0.13
 	./configure && make -j2 && make install
@@ -105,7 +105,7 @@ install_ubuntu_ssr(){
 	apt-get install python-pip git -y
 	pip install cymysql
 	cd /root
-	git clone -b manyuser https://github.com/glzjin/shadowsocks.git
+	git clone -b aead https://github.com/glzjin/shadowsocks.git
 	cd shadowsocks
 	pip install -r requirements.txt
 	chmod +x *.sh
