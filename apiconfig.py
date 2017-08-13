22b6b3830775766af0c90d5b5d6a717087d0c7b9
qinghuas <30293805+qinghuas@users.noreply.github.com>
2017-08-13 22:32:43 +0800

    add apiconfig.py

---
+++ apiconfig.py
@@ -0,0 +1,47 @@
+﻿# Config
+NODE_ID = 1
+
+
+# hour,set 0 to disable
+SPEEDTEST = 6
+CLOUDSAFE = 1
+ANTISSATTACK = 0
+AUTOEXEC = 0
+
+MU_SUFFIX = 'zhaoj.in'
+MU_REGEX = '%5m%id.%suffix'
+
+SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
+API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
+
+WEBAPI_URL = 'https://zhaoj.in'
+WEBAPI_TOKEN = 'glzjin'
+
+# mudb
+MUDB_FILE = 'mudb.json'
+
+# Mysql
+MYSQL_HOST = '127.0.0.1'
+MYSQL_PORT = 3306
+MYSQL_USER = 'ss'
+MYSQL_PASS = 'ss'
+MYSQL_DB = 'shadowsocks'
+
+MYSQL_SSL_ENABLE = 0
+MYSQL_SSL_CA = ''
+MYSQL_SSL_CERT = ''
+MYSQL_SSL_KEY = ''
+
+# API
+API_HOST = '127.0.0.1'
+API_PORT = 80
+API_PATH = '/mu/v2/'
+API_TOKEN = 'abcdef'
+API_UPDATE_TIME = 60
+
+# Manager (ignore this)
+MANAGE_PASS = 'ss233333333'
+# if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+# make sure this port is idle
+MANAGE_PORT = 23333
